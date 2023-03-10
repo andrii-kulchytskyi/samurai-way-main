9537cfc67f4e85b72a96dc211fa0084be2944882
Andrii Kulchytskyi <andrii.kulchytsky@gmail.com>
2023-03-10 23:02:21 +0400

    52

--- src/components/Users/Users.tsx
+++ src/components/Users/Users.tsx
@@ -6,43 +6,45 @@ import userPhoto from "../../assets/images/user.jpg";
 
 
 const Users = (props: UsersContainerPropsType) => {
-
-
-    if (props.users.users.length === 0) {
-        // props.setUsers([
-        //     {
-        //         id: 1,
-        //         photoUrl: "https://s3.amazonaws.com/shecodesio-production/candidates/avatars/000/024/058/medium/AEBA52B5-3261-4D0D-896B-EEF5CE8079C1.jpeg?1652433732",
-        //         followed: false,
-        //         fullName: "Yuliia Sulima",
-        //         status: "Ya bulka bulka bulka",
-        //         location: {city: "Dubai", country: "United Arab Emirates"}
-        //     },
-        //     {
-        //         id: 2,
-        //         photoUrl: "https://s3.amazonaws.com/shecodesio-production/candidates/avatars/000/024/058/medium/AEBA52B5-3261-4D0D-896B-EEF5CE8079C1.jpeg?1652433732",
-        {/*        followed: true,*/
+    const getUsers = () => {
+        if (props.users.users.length === 0) {
+            // props.setUsers([
+            //     {
+            //         id: 1,
+            //         photoUrl: "https://s3.amazonaws.com/shecodesio-production/candidates/avatars/000/024/058/medium/AEBA52B5-3261-4D0D-896B-EEF5CE8079C1.jpeg?1652433732",
+            //         followed: false,
+            //         fullName: "Yuliia Sulima",
+            //         status: "Ya bulka bulka bulka",
+            //         location: {city: "Dubai", country: "United Arab Emirates"}
+            //     },
+            //     {
+            //         id: 2,
+            //         photoUrl: "https://s3.amazonaws.com/shecodesio-production/candidates/avatars/000/024/058/medium/AEBA52B5-3261-4D0D-896B-EEF5CE8079C1.jpeg?1652433732",
+            {/*        followed: true,*/
+            }
+            {/*        fullName: "Rashid Al bin",*/
+            }
+            {/*        status: "Ya sheikh",*/
+            }
+            //         location: {city: "Kharkiv", country: "Ukraine"}
+            //     },
+            //     {
+            //         id: 3,
+            //         photoUrl: "https://s3.amazonaws.com/shecodesio-production/candidates/avatars/000/024/058/medium/AEBA52B5-3261-4D0D-896B-EEF5CE8079C1.jpeg?1652433732",
+            //         followed: false,
+            //         fullName: "Dimych",
+            //         status: "Ya boss",
+            //         location: {city: "Minsk", country: "Belarus"}
+            //     },
+            // ])
         }
-        {/*        fullName: "Rashid Al bin",*/
-        }
-        {/*        status: "Ya sheikh",*/
-        }
-        //         location: {city: "Kharkiv", country: "Ukraine"}
-        //     },
-        //     {
-        //         id: 3,
-        //         photoUrl: "https://s3.amazonaws.com/shecodesio-production/candidates/avatars/000/024/058/medium/AEBA52B5-3261-4D0D-896B-EEF5CE8079C1.jpeg?1652433732",
-        //         followed: false,
-        //         fullName: "Dimych",
-        //         status: "Ya boss",
-        //         location: {city: "Minsk", country: "Belarus"}
-        //     },
-        // ])
+        axios.get("https://social-network.samuraijs.com/api/1.0/users").then((response) => {
+            props.setUsers(response.data.items)
+        })
     }
-    axios.get("https://social-network.samuraijs.com/api/1.0/users").then((response) => {
-        props.setUsers(response.data.items)
-    })
+
     return <div>
+        <button onClick={getUsers}>GET USERS</button>
         {
             props.users.users.map(user => <div key={user.id}>
             <span>
