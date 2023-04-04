e1a91f11400a956f9e5e70957921589432a8d8fe
Andrii Kulchytskyi <andrii.kulchytsky@gmail.com>
2023-04-04 21:41:07 +0400

    71 setState state

--- src/components/Profile/ProfileInfo/ProfileInfo.tsx
+++ src/components/Profile/ProfileInfo/ProfileInfo.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Preloader from "../../common/Preloader";
+import ProfileStatus from "../ProfileStatus";
 
 
 type ProfileInfoType = {
@@ -11,11 +12,9 @@ export const ProfileInfo = (props: ProfileInfoType) => {
     }
     return (
         <div>
-            <div>
-                <img
-                    src="https://www.ikea.com/images/a-small-living-room-area-with-lots-of-light-a-sofa-a-tv-a-ve-976b1c05c23de825421229a2abec9203.jpg"/>
-            </div>
+
             <img src={(props.profile).toString()}/>
+            <ProfileStatus status={"STATUS SHMATUS"}/>
             <div>ava+description</div>
         </div>
     );
